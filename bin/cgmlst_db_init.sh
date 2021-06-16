a8fbde27c3734c8d0e6105b35d8952f53525c6ee
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-06-16 14:20:47 +0200

    Handle arguments

--- bin/cgmlst_db_init.sh
+++ bin/cgmlst_db_init.sh
@@ -12,14 +12,17 @@ usage() {
     exit 1
 }
 
-while getopts ":s:p:" o; do
+while getopts ":c:u:d:" o; do
     case "${o}" in
-        s)
-            s=${OPTARG}
-            ((s == 45 || s == 90)) || usage
+        c)
+            c=${OPTARG}
+            #((s == "Acinetobacter_baumannii" || s == "Enterobacter_cloacae" || s == "Enterococcus_faecalis" || s == "Enterococcus_faecium" || s == "Escherichia_coli" || s == "Klebsiella_pneumoniae" || s == "Mycobacterium_abcessus" || s == "Mycobacterium_avium" || s == "Mycobacterium_intracellulare" || s == "Mycobacterium_malmoense" || s == "Mycobacterium_tuberculosis" || s == "Pseudomonas_aeruginosa" || s == "Staphylococcus_argenteus" || s == "Staphylococcus_aureus" || s == "Stenotrophomonas_maltophilia" || s == "Streptococcus_pyogenes" || s == "Citrobacter_braakii" || s == "Clostridioides_difficile" || s == "Corynebacterium_striatum" || s == "Enterococcus_gallinarum" || s == "Klebsiella_aerogenes" || s == "Mycobacterium_chimera" || s == "Mycobacterium_malmoense" || s == "Mycobacterium_kansasii" || s == "Mycobacterium_chelonae" || s == "Mycobacterium_celatum" || s == "Mycobacterium_marinum" || s == "Mycobacterium_szulgai" || s == "Mycobacterium_gordonae" || s == "Mycobacterium_scrofulaceum" || s == "Mycobacterium_xenopi" || s == "Mycobacterium_africanum" || s == "Mycobacterium_bovis" || s == "Proteus_mirabilis" || s == "Proteus_vulgaris" || s == "Salmonella_enterica")) || usage
             ;;
-        p)
-            p=${OPTARG}
+        u)
+            u=${OPTARG}
+            ;;
+        d)
+            d=${OPTARG}
             ;;
         *)
             usage
