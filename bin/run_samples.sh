038d3e48281cceb5d7311943c7975e193b700e3d
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-07-09 15:28:49 +0200

    Update comments

--- bin/run_samples.sh
+++ bin/run_samples.sh
@@ -7,24 +7,31 @@ set -euo pipefail
 #IFS=$'\n\t'
 
 CWD=$(pwd)
-#echo $CWD
 SEQ_DATA="$CWD"/"assets/sequencing_data"
-#echo $SEQ_DATA
-input=$( find assets/sequencing_data/ -name "*.fastq.gz" -and -type f -print0 | xargs -0 echo )
-read -r -a input_files_array <<< $input
-# Loop through all the isoform csv files
-for i in "${!input_files_array[@]}"; do
-  FULL_FILE_PATH="$CWD"/$(echo ${input_files_array[$i]})
-  RELATIVE_FILE_PATH=$(echo ${input_files_array[$i]})
-  #FILE_PATH=$(echo ${input_files_array[$i]} | cut -d "/" -f3-)
-  INPUT_DIR=$(echo ${input_files_array[$i]} | awk -F/ '{print $3}')
-  FILE_NAME=$(echo ${input_files_array[$i]} | awk -F/ '{print $4}')
-  SAMPLE_STEM=$(basename -s '.fastq.gz' $(echo $FILE_NAME))
-  SAMPLE_ID=$(echo $SAMPLE_STEM | awk -F_ '{print $1}')
-  SPECIES=$(echo "$INPUT_DIR" | awk -F_ '{printf $1"_"$2}')
-  FULL_PATH_INPUT_DIR="$SEQ_DATA"/"$INPUT_DIR"
 
-  nextflow run main.nf -profile local,singularity,"$SPECIES" -resume --sample_ID "$INPUT_DIR"
+# Clear log file for the total run
+rm -f nf_logs.log
+
+input=$( find assets/sequencing_data/ -name "*R1_001.fastq.gz" -and -type f -print0 | xargs -0 echo )
+read -r -a input_files_array <<< $input
+# Loop through all the samples
+for i in "${!input_files_array[@]}"; do
+  # /home/rada/Documents/CGL/JASEN/assets/sequencing_data/Proteus_vulgaris_MRR160070/MRR160070_S14_L001_R1_001.fastq.gz
+  FULL_FILE_PATH="$CWD"/$(echo ${input_files_array[$i]})
+  # assets/sequencing_data/Proteus_vulgaris_MRR160070/MRR160070_S14_L001_R1_001.fastq.gz
+  RELATIVE_FILE_PATH=$(echo ${input_files_array[$i]})
+  # Proteus_vulgaris_MRR160070
+  INPUT_DIR=$(echo ${input_files_array[$i]} | awk -F/ '{print $3}')
+  # MRR160070_S14_L001_R1_001.fastq.gz
+  FILE_NAME=$(echo ${input_files_array[$i]} | awk -F/ '{print $4}')
+  # MRR160070_S14_L001_R1_001
+  SAMPLE_STEM=$(basename -s '.fastq.gz' $(echo $FILE_NAME))
+  # MRR160070
+  SAMPLE_ID=$(echo $SAMPLE_STEM | awk -F_ '{print $1}')
+  # Proteus_vulgaris
+  SPECIES=$(echo "$INPUT_DIR" | awk -F_ '{printf $1"_"$2}')
+  # /home/rada/Documents/CGL/JASEN/assets/sequencing_data/Proteus_vulgaris_MRR160070
+  FULL_PATH_INPUT_DIR="$SEQ_DATA"/"$INPUT_DIR"
   # Clear reference files before running next workflow
   rm -rf assets/references
   #break
