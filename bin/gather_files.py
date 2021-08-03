0bf42600b23cdfed31c05e53923fbfc5a3e244e5
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-08-03 08:34:00 +0200

    Make the script more sustainable

--- bin/gather_files.py
+++ bin/gather_files.py
@@ -1,54 +1,75 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""This script gathers files matched with <pattern> recursively from indir and copies them with <suffix> to outdir."""
 
 import argparse
 from pathlib import Path
 from shutil import copy
 
-# Create the parser
-parser = argparse.ArgumentParser(prog='gather_files.py',
-				    allow_abbrev=False,
-				    usage='%(prog)s [options] <indir> <outdir>',
-				    description='Gather files of interest from <indir> to <outdir> using "pattern" and saving with "suffix"',
-				    epilog='---')
+def filenamer(id, stem, suffix, includeID):
+	"""Form a filename string from id (if includeID == yes) stem and suffix"""
+	if(includeID == 'yes'):
+		return id + "_" + stem + suffix
+	else:
+		return stem + suffix
 
-parser.add_argument('--pattern',
-		    '-p',
-		    type=str,
-		    default='**/motif_report*tsv',
-		    help='Recursive globbing pattern to search files with')
+def make_dir(dir):
+	"""Create a directory if it doesn't yet exist"""
+	if not dir.is_dir():
+		dir.mkdir(parents=True, exist_ok=True)
 
-parser.add_argument('--suffix',
-		    '-s',
-		    type=str,
-		    default='.tsv',
-		    help='File name suffix to use for when saving the copied files')
 
-parser.add_argument('indir',
-		    type=str,
-		    help='Place where all the results tsv:s are spread around')
 
-parser.add_argument('outdir', 
-		    type=str,
-		    help='Place where we want our renamed tsv:s to be stored')
+def main(args):
+	# Path to where the results are
+	all_results = Path(args.indir)
+	# Path where we want our results to end up
+	gathered_tsvs = Path(args.outdir)
+	make_dir(gathered_tsvs)
+	# Find spread out results files and gather them to one output dir
+	for path in sorted(all_results.rglob(args.pattern)):
+		id = path.resolve().parent.parent.name
+		current_file_path = path.resolve()
+		stem = path.stem
+		new_filename = filenamer(id, stem, args.suffix, args.includeID)
+		out_file_path = gathered_tsvs.resolve()/new_filename
+		copy(current_file_path, out_file_path)
 
-# Execute the parse_args() method
-args = parser.parse_args()
+if __name__ == '__main__':
+	# Create the parser
+	parser = argparse.ArgumentParser(prog='gather_files.py',
+					allow_abbrev=False,
+					usage='%(prog)s [options] <indir> <outdir>',
+					description='Gather files of interest from <indir> to <outdir> using "pattern" and saving with "suffix"',
+					epilog='---')
 
-# Path to where the results are
-all_results = Path(args.indir)
-# Path where we want our results to end up
-gathered_tsvs = Path(args.outdir)
+	parser.add_argument('--pattern',
+			'-p',
+			type=str,
+			default='**/motif_report*tsv',
+			help='Recursive globbing pattern to search files with')
 
-# If output dir is missing, create it
-if not gathered_tsvs.is_dir():
-	gathered_tsvs.mkdir(parents=True, exist_ok=True)
+	parser.add_argument('--suffix',
+			'-s',
+			type=str,
+			default='.tsv',
+			help='File name suffix to use for when saving the copied files')
 
-# Find spread out results files and gather them to one output dir
-for path in sorted(all_results.rglob(args.pattern)):
-	id = path.resolve().parent.parent.name
-	motif_report = path.resolve()
-	stem = path.stem
-	new_filename = id + "_" + stem + args.suffix
-	out_file_path = gathered_tsvs.resolve()/new_filename
-	copy(motif_report, out_file_path)
+	parser.add_argument('indir',
+			type=str,
+			help='Place where all the results tsv:s are spread around')
+
+	parser.add_argument('outdir', 	# Path to where the results are
+			type=str,
+			help='Where the gathered files should be copied.')
+
+	parser.add_argument('--includeID',
+			    '-i',
+			    type=str,
+			    default='no',
+			    help='Whether to include ID part in the file name or not')
+
+
+	# Execute the parse_args() method
+	args = parser.parse_args()
+	main(args)
