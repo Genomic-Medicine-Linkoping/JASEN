5c7bb708c20bcf1b8e96740a49253371e33cc2a4
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-08-04 16:01:18 +0200

    Add description

--- bin/join_nf_results.py
+++ bin/join_nf_results.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""This script joins separate tab separated files in various directories into one tsv file."""
 
 import argparse
 from pathlib import Path
