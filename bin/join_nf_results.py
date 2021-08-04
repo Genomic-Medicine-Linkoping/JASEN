bb22f57068c0db09fed2a6223915e1aae6423e6e
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-08-04 16:04:44 +0200

    Use more descriptive name for namedTuple variable

--- bin/join_nf_results.py
+++ bin/join_nf_results.py
@@ -9,45 +9,45 @@ import re
 from collections import namedtuple
 
 def main(args):
-mlst_dir = Path(args.mlst_dir)
-resistance_dir = Path(args.resistance_dir)
+	mlst_dir = Path(args.mlst_dir)
+	resistance_dir = Path(args.resistance_dir)
 	spa_dir = Path(args.spa_dir)
 	ariba_mlst_dir = Path(args.ariba_mlst_dir)
-tsv = Path(args.outfile)
-json_pat = args.json_pattern
+	tsv = Path(args.outfile)
+	json_pat = args.json_pattern
 
-st = {}
+	st = {}
 
-# Store all found MLST types in a dict 
-for path in sorted(mlst_dir.glob(json_pat)):
-	df = pd.read_json(path)
+	# Store all found MLST types in a dict 
+	for path in sorted(mlst_dir.glob(json_pat)):	
+		df = pd.read_json(path)
 		# Extract sequence type
-	sequence_type = df.iloc[0]['sequence_type']
+		sequence_type = df.iloc[0]['sequence_type']
 		# Extract species_sample_id, e.g. Staphylococcus_aureus_SASPA17S-03
-	id = re.sub(json_pat[1:], '', str(path.name))
-	st[id] = str(sequence_type)
+		id = re.sub(json_pat[1:], '', str(path.name))
+		st[id] = str(sequence_type)
 
 	# Namedtuple for keeping track of resistance genes and species and genes
-Resistance_genes = namedtuple('Resistance_genes', ['species_id','genes'])
-# List for storing species_id specific NamedTuples  
-species_id_res_genes_list = []
+	Resistance_genes = namedtuple('Resistance_genes', ['species_id','genes'])
+	# List for storing species_id specific NamedTuples
+	species_id_res_genes_list = []
 
 	# Collect all ariba resistance gene findings to a list of NamedTuples
-for path in sorted(resistance_dir.glob(args.res_pattern)):
-	df = pd.read_csv(path, sep='\t')
+	for path in sorted(resistance_dir.glob(args.res_pattern)):
+		df = pd.read_csv(path, sep='\t')
 		# Extract species_id data from file name, e.g. Staphylococcus_aureus_SASPA17S-03
-	species_id = re.sub(r'_motif_report.*\.tsv', '', str(path.name))
+		species_id = re.sub(r'_motif_report.*\.tsv', '', str(path.name))
 		# Convert all genes in the current dataframe to a list and then to a set
 		genes = set(df["ref_name"].to_list())
 		# Check if the species_id_res_genes_list contains already the current species_id inside a namedtuple
-	if (any(nt.species_id==species_id for nt in species_id_res_genes_list)):
-		# Obtain already in list stored NamedTuple instance
-		current_nt = [nt for nt in species_id_res_genes_list if nt.species_id==species_id][0]
-		# Use sets to store gene names so that only unique genes are stored
-		current_nt.genes.update(genes)
-	else:
-		# If the NamedTuple with specific species_id doesn't exist in the list yet,
-		# add it to the list with genes inside a set
+		if (any(nt.species_id==species_id for nt in species_id_res_genes_list)):
+			# Obtain already in list stored NamedTuple instance
+			current_nt = [nt for nt in species_id_res_genes_list if nt.species_id==species_id][0]
+			# Use sets to store gene names so that only unique genes are stored
+			current_nt.genes.update(genes)
+		else:
+			# If the NamedTuple with specific species_id doesn't exist in the list yet,
+			# add it to the list with genes inside a set
 			species_id_res_genes_list.append(Resistance_genes(species_id, genes))
 
 	spa_types = {}
@@ -70,26 +70,30 @@ for path in sorted(resistance_dir.glob(args.res_pattern)):
 		ariba_mlst_type = df.iloc[0,0]
 		ariba_mlst[species_id] = ariba_mlst_type
 
-with open(tsv, 'w') as f:
-	# Write tsv header
-	f.write(f'Prov-nummer\tArt\tKända resistensgener samt PVL\tKänd MLST eller Spa-typ\n')
+	with open(tsv, 'w') as f:
+		# Write tsv header
+		f.write(f'Prov-nummer\tArt\tKända resistensgener samt PVL\tKänd MLST eller Spa-typ\n')
+		for current_si_nt in species_id_res_genes_list:
 			# Extract mlst-type from st dict
+			mlst_type = st.get(current_si_nt.species_id)
+			si = current_si_nt.species_id.split('_')
 			# Extract current species and shorten the name, e.g. S. aureus
-		species = si[0][0] + '. ' + si[1]
+			species = si[0][0] + '. ' + si[1]
 			# Extract current sample id, e.g. SASPA17S-03
-		id = si[2:][0]
+			id = si[2:][0]
 			# Join all ariba resistance genes into one "," separated string 
+			genes_str = ', '.join(current_si_nt.genes)
 			# Extract spa-type if it exists
 			spa_type = spa_types.get(current_si_nt.species_id)
 			# Extract ariba mlst-type
 			ariba_mlst_type = ariba_mlst.get(current_si_nt.species_id)
 			
-		# Write nothing if MLST type is missing
-		if (mlst_type=='-'):
+			# Write nothing if MLST type is missing
+			if (mlst_type=='-'):
 				# Do we write spa-type or not when MLST type is missing
 				if (not spa_type):
 					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}\n')
-		else:
+				else:
 					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, {spa_type}\n')
 			else:
 				# Do we write spa-type or not when MLST type is found
