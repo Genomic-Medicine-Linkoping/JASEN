1f9052888aec70b8b00b8713b6f68cf9817a1edd
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-08-04 16:04:10 +0200

    Add comments

--- bin/join_nf_results.py
+++ bin/join_nf_results.py
@@ -21,21 +21,25 @@ st = {}
 # Store all found MLST types in a dict 
 for path in sorted(mlst_dir.glob(json_pat)):
 	df = pd.read_json(path)
+		# Extract sequence type
 	sequence_type = df.iloc[0]['sequence_type']
+		# Extract species_sample_id, e.g. Staphylococcus_aureus_SASPA17S-03
 	id = re.sub(json_pat[1:], '', str(path.name))
 	st[id] = str(sequence_type)
 
-# Define the namedtuple
+	# Namedtuple for keeping track of resistance genes and species and genes
 Resistance_genes = namedtuple('Resistance_genes', ['species_id','genes'])
 # List for storing species_id specific NamedTuples  
 species_id_res_genes_list = []
 
-# 
+	# Collect all ariba resistance gene findings to a list of NamedTuples
 for path in sorted(resistance_dir.glob(args.res_pattern)):
 	df = pd.read_csv(path, sep='\t')
-	# Extract species and ID data from file name
+		# Extract species_id data from file name, e.g. Staphylococcus_aureus_SASPA17S-03
 	species_id = re.sub(r'_motif_report.*\.tsv', '', str(path.name))
+		# Convert all genes in the current dataframe to a list and then to a set
 		genes = set(df["ref_name"].to_list())
+		# Check if the species_id_res_genes_list contains already the current species_id inside a namedtuple
 	if (any(nt.species_id==species_id for nt in species_id_res_genes_list)):
 		# Obtain already in list stored NamedTuple instance
 		current_nt = [nt for nt in species_id_res_genes_list if nt.species_id==species_id][0]
@@ -69,11 +73,12 @@ for path in sorted(resistance_dir.glob(args.res_pattern)):
 with open(tsv, 'w') as f:
 	# Write tsv header
 	f.write(f'Prov-nummer\tArt\tKända resistensgener samt PVL\tKänd MLST eller Spa-typ\n')
-	for current_si in species_id_res_genes_list:
-		mlst_type = st.get(current_si.species_id)
-		si = current_si.species_id.split('_')
+			# Extract mlst-type from st dict
+			# Extract current species and shorten the name, e.g. S. aureus
 		species = si[0][0] + '. ' + si[1]
+			# Extract current sample id, e.g. SASPA17S-03
 		id = si[2:][0]
+			# Join all ariba resistance genes into one "," separated string 
 			# Extract spa-type if it exists
 			spa_type = spa_types.get(current_si_nt.species_id)
 			# Extract ariba mlst-type
