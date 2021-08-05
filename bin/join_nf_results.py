828d850593f8e7f9c2129afc3fd8323e1f35369c
LJMesi <37740329+ljmesi@users.noreply.github.com>
2021-08-05 08:28:38 +0200

    Add AMRFinderPlus results joining to the script

--- bin/join_nf_results.py
+++ bin/join_nf_results.py
@@ -15,9 +15,9 @@ def main(args):
 	ariba_mlst_dir = Path(args.ariba_mlst_dir)
 	tsv = Path(args.outfile)
 	json_pat = args.json_pattern
+	amrfinderplus_dir = Path(args.amrfinderplus_dir)
 
 	st = {}
-
 	# Store all found MLST types in a dict 
 	for path in sorted(mlst_dir.glob(json_pat)):	
 		df = pd.read_json(path)
@@ -27,11 +27,11 @@ def main(args):
 		id = re.sub(json_pat[1:], '', str(path.name))
 		st[id] = str(sequence_type)
 
+
 	# Namedtuple for keeping track of resistance genes and species and genes
 	Resistance_genes = namedtuple('Resistance_genes', ['species_id','genes'])
 	# List for storing species_id specific NamedTuples
 	species_id_res_genes_list = []
-
 	# Collect all ariba resistance gene findings to a list of NamedTuples
 	for path in sorted(resistance_dir.glob(args.res_pattern)):
 		df = pd.read_csv(path, sep='\t')
@@ -50,8 +50,24 @@ def main(args):
 			# add it to the list with genes inside a set
 			species_id_res_genes_list.append(Resistance_genes(species_id, genes))
 
-	spa_types = {}
 
+	# Dict for storing species_id and genes with their scope
+	species_id_amrFinderPlus = {}
+	# Collect all ariba resistance gene findings to a list of NamedTuples
+	for path in sorted(amrfinderplus_dir.glob(args.amrfp_pattern)):
+		df = pd.read_csv(path, sep='\t')
+		df = df[df['Scope'] == 'core']
+		#df['Gene scope'] = df['Gene symbol'] + ' (' + df['Scope'] + ')'
+
+		# Extract species_id data from file name, e.g. Staphylococcus_aureus_SASPA17S-03
+		species_id = re.sub('_aMRFinderPlus', '', str(path.stem))
+		# Convert all genes in the current dataframe to a list and then to a set
+		#gene_scope = df["Gene scope"].to_list()
+		gene_symbols = df["Gene symbol"].to_list()
+		species_id_amrFinderPlus[species_id] = gene_symbols
+
+
+	spa_types = {}
 	for path in sorted(spa_dir.glob(args.spa_typer_pattern)):
 		df = pd.read_csv(path, sep='\t')
 		# Extract species_id data from file name, e.g. Staphylococcus_aureus_SASPA17S-03
@@ -60,8 +76,8 @@ def main(args):
 		spa_type = df.iloc[0,2]
 		spa_types[species_id] = spa_type
 
-	ariba_mlst = {}
 
+	ariba_mlst = {}
 	for path in sorted(ariba_mlst_dir.glob(args.ariba_mlst_pattern)):
 		df = pd.read_csv(path, sep='\t')
 		# Extract species_id data from file name, e.g. Staphylococcus_aureus_SASPA17S-03
@@ -70,9 +86,10 @@ def main(args):
 		ariba_mlst_type = df.iloc[0,0]
 		ariba_mlst[species_id] = ariba_mlst_type
 
+
 	with open(tsv, 'w') as f:
 		# Write tsv header
-		f.write(f'Prov-nummer\tArt\tKända resistensgener samt PVL\tKänd MLST eller Spa-typ\n')
+		f.write(f'Prov-nummer\tArt\tKända resistensgener samt PVL\tKänd MLST eller Spa-typ\tAMRFinderPlus gener\n')
 		for current_si_nt in species_id_res_genes_list:
 			# Extract mlst-type from st dict
 			mlst_type = st.get(current_si_nt.species_id)
@@ -88,19 +105,21 @@ def main(args):
 			# Extract ariba mlst-type
 			ariba_mlst_type = ariba_mlst.get(current_si_nt.species_id)
 			
+			gene_symbols = ', '.join(species_id_amrFinderPlus.get(current_si_nt.species_id))
+
 			# Write nothing if MLST type is missing
 			if (mlst_type=='-'):
 				# Do we write spa-type or not when MLST type is missing
 				if (not spa_type):
-					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}\n')
+					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}\t{gene_symbols}\n')
 				else:
-					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, {spa_type}\n')
+					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, {spa_type}\t{gene_symbols}\n')
 			else:
 				# Do we write spa-type or not when MLST type is found
 				if (not spa_type):
-					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, ST{mlst_type}\n')
+					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, ST{mlst_type}\t{gene_symbols}\n')
 				else:
-					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, ST{mlst_type}, {spa_type}\n')
+					f.write(f'{id}\t{species}\t{genes_str}\taST{ariba_mlst_type}, ST{mlst_type}, {spa_type}\t{gene_symbols}\n')
 
 
 
@@ -109,7 +128,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(prog='join_nf_results.py',
 									 allow_abbrev=False,
 									 usage='%(prog)s [options] <mlst_dir> <resistance_dir> <outfile>',
-									 description='',
+									 description=__doc__,
 									 epilog='---')
 
 	parser.add_argument('mlst_dir',
@@ -127,6 +146,10 @@ if __name__ == '__main__':
 	parser.add_argument('ariba_mlst_dir',
 			type=str,
 			help='Place where all the ariba mlst results are')
+
+	parser.add_argument('amrfinderplus_dir',
+			type=str,
+			help='Place where all the AMRFinderPlus output tsv results files are')
 
 	parser.add_argument('outfile', 
 			type=str,
@@ -155,6 +178,12 @@ if __name__ == '__main__':
 			type=str,
 			default='*_mlst_report.tsv',
 			help='Globbing pattern to search ariba mlst tsv files with')
+
+	parser.add_argument('--amrfp-pattern',
+			'-m',
+			type=str,
+			default='*_aMRFinderPlus.tsv',
+			help='Globbing pattern to search AMRFinderPlus tsv files with')
 
 	# Execute the parse_args() method
 	args = parser.parse_args()
